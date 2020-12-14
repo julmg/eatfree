3fcfc00d004f436a10d7088d2e0ed177f1cd72b5
SurvivalSlayer <nathan.lefebvre@live.fr>
2020-12-14 10:54:40 +0100

    Update VuePanel.java

--- app/src/main/java/com/example/eatfree/comparaison/VuePanel.java
+++ app/src/main/java/com/example/eatfree/comparaison/VuePanel.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 
@@ -15,7 +17,11 @@ import java.util.Observer;
 
 public class VuePanel extends LinearLayout implements Observer {
 
+    //! Affichage des allergènes qui on une correspondance
+    public TextView txt_Allergene_correspondant;
 
+    //! Affichage du pouce vert ou rouge
+    public ImageView img_pouce;
 
     //! \brief constructeur de la classe VuePanel
     //! il lie le fichier panel_allergene_check.xml, responsable du visuel, aux fichiers qui gèrent l'affichage des données
@@ -25,6 +31,13 @@ public class VuePanel extends LinearLayout implements Observer {
 
         LayoutInflater inflater = ((Activity)context).getLayoutInflater();
         inflater.inflate(R.layout.panel_allergene_check, this);
+
+        txt_Allergene_correspondant = (TextView)findViewById(R.id.txtAllergene);
+        img_pouce = (ImageView)findViewById(R.id.img_pouce);
+
+        /*
+        * set le txt_Allergene_correspondant avec ce que retourne le controlleur de batiste
+        * */
 
     }
 
